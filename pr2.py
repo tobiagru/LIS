d7a86b45e629338070bc4f71f384117474c3fd4f
tobiagru <tobiagru@student.ethz.ch>
2016-03-31 01:07:03 +0200

    finished import/export, added todos for classification

--- pr2.py
+++ pr2.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
+import sklearn as sl
 
-
+# import/export functions --------------------------------------------------------------------
 def load_Y(fname, asNpArray = False):
      data = pd.read_csv(fname,
-                       index_col=1,
+                       index_col=0,
                        dtype=np.float32,
                        header=0,
-                       usecols = [1])
+                       usecols = [0,1])
      if asNpArray:
          return data.as_matrix
      else:
@@ -15,10 +16,10 @@ def load_Y(fname, asNpArray = False):
 
 def load_X_train(fname,asNpArray = False):
     data = pd.read_csv(fname,
-                       index_col=1,
+                       index_col=0,
                        dtype=np.float32,
                        header=0,
-                       usecols = range(2,16,1))
+                       usecols = [0] + list(range(2,17,1)))
     if asNpArray:
         return data.as_matrix
     else:
@@ -27,34 +28,44 @@ def load_X_train(fname,asNpArray = False):
 
 def load_X_test(fname, asNpArray = False):
     data = pd.read_csv(fname,
-                       index_col=1,
+                       index_col=0,
                        dtype=np.float32,
                        header=0,
-                       usecols = range(1,15,1))
+                       usecols = range(0,16,1))
     if asNpArray:
         return data.as_matrix
     else:
         return data
 
-#print pandas dataframe as np.array with dataframe.as_matrix
+def create_Y(Y_pred,X_test):
+    try:
+        Y_pred.shape[0] is not X_test.as_matrix().shape[0]
+    except:
+        print("error - dimension of y matrix does not match number of expected predictions")
+    else:
+        return pd.DataFrame(data = Y_pred, index = X_test.index, columns = ['y'])
 
-def write_Y(fname, Y_pred, X_test):
-    if Y_pred.as_matrix.shape[1] is not X_test.as_matrix.shape[1]:
-        print
-    data = pd.DataFrame(data = Y_pred,
-                        index = X_test.index,
-                        columns = ['Id','y'])
+def write_Y(fname, Y_pred, X_test = 0):
+    if X_test is not 0:
+        data = create_Y(Y_pred,X_test)
     f = open(fname, 'w')
     data.to_csv(f)
 
+#prediction functions -------------------------------------------------------------------------
 
 
-X_train = load_X_train("/home/tg/Projects/Data/pr2/train.csv")
-Y = load_Y("/home/tg/Projects/Data/pr2/train.csv")
-X_test = load_X_test("/home/tg/Projects/Data/pr2/test.csv")
-y_z = np.ones([X_test.shape[1],1])
-write_Y("/home/tg/Projects/Data/pr2/handin_test.csv",y_z,X_test)
+#TODO - Scorefunction
 
-print(X_train.index)
-print(X_train.columns)
-print(np.shape())
+#TODO - Crossval
+#TODO - feature Selection
+#TODO - pipeline
+
+#TODO - SVM (RBF, Gaussian, Linear)
+
+#TODO - DecisionTrees
+
+#TODO - NearestNeighbour
+
+#TODO - Stochastic Gradient Decent
+
+

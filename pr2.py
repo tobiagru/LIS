5c120df8243c56cdaf084e272026144bc9561ea6
tobiagru <tobiagru@student.ethz.ch>
2016-04-02 12:30:00 +0200

    latest upload

--- pr2.py
+++ pr2.py
@@ -9,20 +9,23 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, ExtraTreesClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 import datetime
+from sklearn.feature_selection import SelectFromModel
 
 
-names = ["Nearest_Neighbors",
+names = [#"Nearest_Neighbors",
          #"Linear_SVM",
          #"RBF_SVM",
          #"Poly_SVM",
          #"Sigmoid_SVM",
          #"Decision_Tree",
-         "Random_Forest",
+         #"Random_Forest",
+         "Extra_Trees",
+         "Gradient_Boosting"
          #"AdaBoost",
          #"Naive_Bayes",
          #"Linear_Discriminant_Analysis",
@@ -30,26 +33,46 @@ names = ["Nearest_Neighbors",
         ]
 
 classifiers = [
-    KNeighborsClassifier(),
+    #KNeighborsClassifier(),
     #SVC(kernel="linear"),
     #SVC(kernel='rbf'),
     #SVC(kernel='poly'),
     #SVC(kernel='sigmoid'),
     #DecisionTreeClassifier(max_depth=5),
-    RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+    #RandomForestClassifier(max_features=2),
+    ExtraTreesClassifier(),
+    GradientBoostingClassifier()
     #AdaBoostClassifier(),
     #GaussianNB(),
     #LinearDiscriminantAnalysis(),
     #QuadraticDiscriminantAnalysis()
     ]
 
-param_grid = [{'n_neighbors': np.arange(3,20,dtype=np.int16),'leaf_size':np.arange(7,15,dtype=np.int16)},
+param_grid = [#{'n_neighbors': np.arange(3,150,15,dtype=np.int16),'leaf_size':np.arange(5,20,dtype=np.int16)},
               #{'C': np.linspace(0.05,1,20)},
               #{'C': np.linspace(0.05,1,20), 'gamma': np.linspace(0.05,5,20)},
-              #{'C': np.linspace(0.05,1,20), 'gamma': np.linspace(0.05,5,20), 'degree': np.arange(2,4,dtype=np.int16)},
+              #{'C': np.linspace(0.05,1,5), 'gamma': np.linspace(0.05,5,5), 'degree': np.arange(2,4,dtype=np.int16)},
               #{'C': np.linspace(0.05,1,20), 'gamma': np.linspace(0.05,5,20)},
               #{'max_depth': np.arange(3,10,dtype=np.int16), 'max_features': np.arange(1,3,dtype=np.int16)},
-              {'max_depth': np.arange(3,15,dtype=np.int16), 'n_estimators': np.arange(10,400,10,dtype=np.int16), 'max_features': np.arange(1,3,dtype=np.int16)},
+              #{'max_depth': np.arange(8,15,dtype=np.int16), 'n_estimators': np.arange(300,400,10,dtype=np.int16)},
+              {'criterion': ['gini','entropy'],
+               'max_features': [2,cd'auto','sqrt','log2'],
+               'max_depth': np.arange(5,15,dtype=np.int16),
+               'n_estimators': np.arange(150,700,50,dtype=np.int16),
+               'min_samples_split': np.arange(1,3,dtype=np.int16),
+               'min_samples_leaf':np.arange(1,5,dtype=np.int16),
+               'min_weight_fraction_leaf': np.linspace(0.05,0.5,5),
+               'bootstrap': [False,True],
+               #'oob_score': [False,True],
+               'class_weight':['balanced','balanced_subsample',None]},
+              {'learning_rate': np.linspace(0.5,0.9,4),
+               'max_features': [2,'auto','sqrt','log'],
+               'n_estimators': np.arange(200,700,50,dtype=np.int32),
+               'max_depth': np.arange(2,15,3,dtype=np.int16),
+               'min_samples_split': np.arange(1,3,dtype=np.int16),
+               'min_samples_leaf':np.arange(1,5,dtype=np.int16),
+               'min_weight_fraction_leaf': np.linspace(0.05,0.5,5),
+               'subsample': np.linspace(0.7,1.3,0.1)}
               #{'n_estimators': np.arange(30,150,10,dtype=np.int16)},
               #{},
               #{},
@@ -75,18 +98,18 @@ for name, clf, param in zip(names, classifiers, param_grid):
         print('\ntesting classifier {0} start at {1}'.format(name,date_time))
 
         #normalize
-        scaler = StandardScaler().fit(X_train)
-        X_train = scaler.transform(X_train)
-        X_test = scaler.transform(X_test)
-
-        #feature select
+        #print("-> Normalize vectors")
+        #scaler = StandardScaler().fit(X_train)
+        #X_train = scaler.transform(X_train)
+        #X_test = scaler.transform(X_test)
 
 
         #train all options
+        print("-> do Gridsearch")
         if name is "Naive_Bayes" or name is "Linear_Discriminant_Analysis" or name is "Quadratic_Discriminant_Analysis":
             #if no gridsearch neccessary just do cross validation
             try:
-                score = cross_val_score(clf, X_train, y_train, scoring='accuracy',cv=10,n_jobs=-1)
+                score = cross_val_score(clf, X_train, y_train, scoring='accuracy',cv=10,n_jobs=1)
             except:
                 print('error - Problem while doing cross validation with {0}'.format(name))
                 traceback.print_exc(file=sys.stdout)
@@ -99,7 +122,7 @@ for name, clf, param in zip(names, classifiers, param_grid):
         else:
             #if gridsearch necessary do it and print best solution
             try:
-                grid_search = GridSearchCV(clf, param, scoring='accuracy',cv=10,n_jobs=-1)
+                grid_search = GridSearchCV(clf, param, scoring='accuracy',cv=10,n_jobs=1)
                 grid_search.fit(X_train, y_train)
             except:
                 print('error - Problem while doing gridsearch with {0}'.format(name))
@@ -115,7 +138,23 @@ for name, clf, param in zip(names, classifiers, param_grid):
             print('-> {0} scored {1:.4f} with param: {2}'.format(name,score,best_param))
             lib_IO.log_best_param_score("/home/tg/Projects/LIS/Data/pr2/log.txt",date_time,name,score,best_param)
 
-        print("-> finished CV starting with full training and prediction")
+        #feature select
+        #print("-> feature Select from vectors")
+        #threshold_y1 = "0.10*mean"
+        #threshold_y2 = "0.06*mean"
+        #trf1 = clf_tmp
+        #trf1.fit(X_train,y_train)
+        #try:
+        #    selection = SelectFromModel(trf1, threshold= threshold_y1, prefit=True)
+        #    X_train = selection.transform(X_train)
+        #    X_test = selection.transform(X_test)
+        #except:
+        #    print('error - no selection method available with classifier')
+        #    traceback.print_exc(file=sys.stdout)
+        #print('-> number of features used after selection {0}'.format(np.shape(X_train)[1]))
+
+
+        print("-> starting with full training and prediction")
 
         try:
             clf_tmp.fit(X_train, y_train)

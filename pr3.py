61a3a3b7ff968663df7378e9e95ccde693803e85
tobiagru <tobiagru@student.ethz.ch>
2016-05-07 13:23:29 +0200

    ...

--- pr3.py
+++ pr3.py
@@ -148,47 +148,11 @@ for type in ["MLP",]:
                     "activation": activation,
                     "input_dim": 100
                     },
-                    #{
-                    #"layer": "DropOut",
-                    ##Dropout
-                    #"p": 0.5,
-                    #},
-                    {
-                    "layer": "Dense",
-                    #Dense
-                    "output_dim": nb_neuron,
-                    #Dense & Conv
-                    "activation": activation,
-                    },
-                    #{
-                    #"layer": "DropOut",
-                    ##Dropout
-                    #"p": 0.4,
-                    #},
-                    {
-                    "layer": "Dense",
-                    #Dense
-                    "output_dim": nb_neuron,
-                    #Dense & Conv
-                    "activation": activation,
-                    },
                     {
                     "layer": "DropOut",
                     #Dropout
-                    "p": 0.4,
+                    "p": 0.5,
                     },
-                    {
-                    "layer": "Dense",
-                    #Dense
-                    "output_dim": nb_neuron,
-                    #Dense & Conv
-                    "activation": activation,
-                    },
-                    #{
-                    #"layer": "DropOut",
-                    ##Dropout
-                    #"p": 0.4,
-                    #},
                     {
                     "layer": "Dense",
                     #Dense

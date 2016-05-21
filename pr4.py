f651c43727dad17b9b302a85a6875fc8db479f34
tobiagru <tobiagru@student.ethz.ch>
2016-05-22 01:57:26 +0200

    ...

--- pr4.py
+++ pr4.py
@@ -18,13 +18,13 @@ train_labeled = pd.read_hdf("Data/pr4/train_labeled.h5", "train")
 train_unlabeled = pd.read_hdf("Data/pr4/train_unlabeled.h5", "train")
 test = pd.read_hdf("Data/pr4/test.h5", "test")
 
-train, valid = train_test_split(train_labeled._values, test_size = 0.14, random_state = 17)
+train, valid = train_test_split(train_labeled._values, test_size = 0.135, random_state = 17)
 
 X_train = np.zeros(((21000 + train.shape[0]),128))
 X_train[:train.shape[0]] = train[:, 1:129]
 X_train[train.shape[0]:] = train_unlabeled._values[:, 0:128]
 
-y_train = np.ones((29000)) *(-1)
+y_train = np.ones((21000 + train.shape[0],)) *(-1)
 y_train[:train.shape[0]] = train[:, 0]
 
 X_valid = valid[:,1:129]

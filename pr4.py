d9d9de0296f599ecdf20b211eacca5fb88d4635b
tobiagru <tobiagru@student.ethz.ch>
2016-05-22 01:52:13 +0200

    ...

--- pr4.py
+++ pr4.py
@@ -1,7 +1,9 @@
 #project 4 start
 import numpy as np
 from sklearn.semi_supervised import LabelPropagation, LabelSpreading
-from sklearn.grid_search import GridSearchCV
+from sklearn.grid_search import GridSearchCV, ParameterGrid
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import accuracy_score
 import pandas as pd
 import lib_IO
 import datetime
@@ -16,13 +18,17 @@ train_labeled = pd.read_hdf("Data/pr4/train_labeled.h5", "train")
 train_unlabeled = pd.read_hdf("Data/pr4/train_unlabeled.h5", "train")
 test = pd.read_hdf("Data/pr4/test.h5", "test")
 
-X_train = np.zeros((30000,128))
-X_train[:9000] = train_labeled._values[:, 1:129]
-X_train[9000:] = train_unlabeled._values[:, 0:128]
+train, valid = train_test_split(train_labeled._values, test_size = 0.125, random_state = 17)
 
-y_train = np.ones((30000)) *(-1)
-y_train[:9000] = train_labeled._values[:, 0]
+X_train = np.zeros((29000,128))
+X_train[:train.shape[0]] = train[:, 1:129]
+X_train[train.shape[0]:] = train_unlabeled._values[:, 0:128]
 
+y_train = np.ones((29000)) *(-1)
+y_train[:train.shape[0]] = train[:, 0]
+
+X_valid = valid[:,1:129]
+y_valid = valid[:,0]
 #k-means
 #PCA
 #bayes decision theory
@@ -32,7 +38,7 @@ y_train[:9000] = train_labeled._values[:, 0]
 #Pipeline
 
 #paramgrid
-param_grid = [
+params = [
              {
                  "kernel": ['rbf',],
                  "gamma": np.logspace(0.00001,100, 8),
@@ -47,26 +53,55 @@ param_grid = [
              },
              ]
 
-names = ["propagation", "spreading"]
-clf1 = LabelPropagation()
-clf2 = LabelSpreading()
-clfs = [clf1, clf2]
+param_grid = list(ParameterGrid(params))
 
-for name, clf in zip(names,clfs):
-    for param in param_grid:
+names = ["propagation", "spreading"]
+
+for param in param_grid:
+    for name in names:
+        if param["kernel"] == 'rbf':
+            if name == "propagation":
+                clf = LabelPropagation(kernel=param["kernel"],
+                                       gamma=["gamma"],
+                                       alpha=param["alpha"],
+                                       max_iter=param["max_iter"])
+            else:
+                clf = LabelSpreading(kernel=param["kernel"],
+                                       gamma=["gamma"],
+                                       alpha=param["alpha"],
+                                       max_iter=param["max_iter"])
+        else:
+            if name == "propagation":
+                clf = LabelPropagation(kernel=param["kernel"],
+                                       n_neighbors=param["n_neighbors"],
+                                       alpha=param["alpha"],
+                                       max_iter=param["max_iter"])
+            else:
+                clf = LabelSpreading(kernel=param["kernel"],
+                                       n_neighbors=param["n_neighbors"],
+                                       alpha=param["alpha"],
+                                       max_iter=param["max_iter"])
+
         now = datetime.datetime.now()
         date_time = '{0:02d}_{1:02d}_{2:02d}_{3:02d}_{4:02d}'.format((now.year%2000),
                                                                      now.month, now.day,
                                                                      now.hour, now.minute)
         logging.info("start with {0}".format(name))
+
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_valid)
+        score = accuracy_score(y_valid, y_pred, False)
+
         #classification Type
         #ovo_clf = OneVsOneClassifier(clf)
         #ovr_clf = OneVsRestClassifier(clf)
 
         #Gridsearch
-        grid_search = GridSearchCV(clf, param, scoring='accuracy',cv=10, n_jobs=-1, verbose=1)
-        grid_search.fit(X_train, y_train)
-        clf_tmp = grid_search.best_estimator_
-        score = grid_search.best_score_
-        best_param = grid_search.best_params_
-        lib_IO.log_best_param_score(datetime,name,score,best_param)
+        #grid_search = GridSearchCV(clf, param, scoring='accuracy',cv=10, n_jobs=-1, verbose=1)
+        #grid_search.fit(X_train, y_train)
+
+        #clf_tmp = grid_search.best_estimator_
+        #score = grid_search.best_score_
+        #best_param = grid_search.best_params_
+
+        lib_IO.log_best_param_score(datetime,name,score,param)
